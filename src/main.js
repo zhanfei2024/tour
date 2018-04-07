4e73b8a3bb007846efce460b3c94281aaf874b87
zhanfei <2024539581@qq.com>
2018-04-07 22:49:37 +0800

    add router and home auth module

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from'iview'
+import 'iview/dist/styles/iview.css'
+import './assets/css/full-layout.css'
+
+
+
 
 Vue.config.productionTip = false
 Vue.use(iView)

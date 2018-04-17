effcb2f3babe917b63bd56c8b0cc3199e4be7d8e
zhanfei <2024539581@qq.com>
2018-04-17 23:52:00 +0800

    修复 订单详情页，添加出行人功能

--- src/views/travel/line-form/line-form.vue
+++ src/views/travel/line-form/line-form.vue
@@ -47,30 +47,31 @@
                         <div class="custom-card product-info">
                             <h1 class="card-title">出行人信息<span>为了确保您顺利出行，请务必仔细填写出行人的信息，避免因信息错误耽误您的旅行</span></h1>
                             <div class="card-box">
-                                <Form v-for="form in selectTravelForm" :key="form.formId" ref="travelForm" :model="form.travelForm" :rules="form.travelValidate" :label-width="80">
+                                <Form v-for="form in selectTravelForm" :key="form.id" ref="travelForm" :model="form.form.travelForm" :rules="form.form.travelValidate" :label-width="80">
+                                    <h1>第{{form.id + 1}}位出行人</h1>
                                     <FormItem label="姓名" prop="name">
-                                        <Input v-model="form.travelForm.name" placeholder="Enter your name"></Input>
+                                        <Input v-model="form.form.travelForm.name" placeholder="Enter your name"></Input>
                                     </FormItem>
                                     <FormItem label="证件类型" prop="idType">
-                                        <Select v-model="form.travelForm.idType" placeholder="Select your id">
+                                        <Select v-model="form.form.travelForm.idType" placeholder="Select your id">
                                             <Option value="beijing">New York</Option>
                                             <Option value="shanghai">London</Option>
                                             <Option value="shenzhen">Sydney</Option>
                                         </Select>
                                     </FormItem>
                                     <FormItem label="证件号码" prop="idNum">
-                                        <Input v-model="form.travelForm.idNum" placeholder="Enter your id number"></Input>
+                                        <Input v-model="form.form.travelForm.idNum" placeholder="Enter your id number"></Input>
                                     </FormItem>
                                     <FormItem label="手机号" prop="phone">
-                                        <Input v-model="form.travelForm.phone" placeholder="Enter your phone"></Input>
+                                        <Input v-model="form.form.travelForm.phone" placeholder="Enter your phone"></Input>
                                     </FormItem>
                                 </Form>
                             </div>
                         </div>
                         
                         <!-- 确认提交订单 -->
-                        <div class="line-form-submit">
-                            <Button type="primary">Primary</Button>
+                        <div class="line-form-submit text-center">
+                            <Button type="primary">保存</Button>
                         </div>
                         <!-- <FormItem>
                             <Button type="primary" @click="handleSubmit('')">Submit</Button>
@@ -92,7 +93,6 @@ export default {
     return {
         selectNum: 0,
         Form: {
-            formId: '',
             travelForm: {
                         name: '',
                         idType: '',
@@ -108,6 +108,9 @@ export default {
                         ],
                         idNum: [
                             { required: true, message: 'The id number cannot be empty', trigger: 'blur' }
+                        ],
+                        phone: [
+                            { required: true, message: 'The phone cannot be empty', trigger: 'blur' }
                         ],
             },
         },
@@ -130,12 +133,17 @@ export default {
     }
   },
   created() {
-      console.log(this.selectTravelForm.push(this.Form), this.Form, '----------')
+    //   console.log(this.selectTravelForm.push(this.Form), this.Form, '----------')
   },
   watch: {
       selectNum: function (newSelectNum, oldSelectNum) {
-          console.log(newSelectNum, oldSelectNum)
-          this.selectTravelForm.push(this.Form)
+          let direction = newSelectNum - oldSelectNum
+          if (direction>0) {
+              this.selectTravelForm.push({form: this.Form, id: oldSelectNum})
+          } else {
+              this.selectTravelForm.pop({form: this.Form, id: newSelectNum})
+          }
+          console.log(this.selectTravelForm, '---------')
       }
   },
   components: {
